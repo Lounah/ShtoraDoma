dd4e2c79bc87f0c3a723f7b26aefe599e8656b3f
Lounah <30981413+Lounah@users.noreply.github.com>
2018-03-09 14:34:15 +0300

    settings fragment created

--- app/src/main/java/com/lounah/silkapp/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/lounah/silkapp/ui/settings/SettingsFragment.java
@@ -5,16 +5,24 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lounah.silkapp.R;
 import com.lounah.silkapp.ui.BaseFragment;
+import com.lounah.silkapp.ui.MainActivity;
 import com.lounah.silkapp.ui.dialogs.DialogsFragment;
 import com.lounah.silkapp.ui.dialogs.DialogsViewModel;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SettingsFragment extends BaseFragment {
+
+    @BindView(R.id.settings_toolbar)
+    Toolbar toolbar;
 
     @Override
     public void onAttach(Context context) {
@@ -32,6 +40,8 @@ public class SettingsFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+        ((MainActivity) getActivity()).onUpdateToolbar(toolbar);
         return view;
     }
 
